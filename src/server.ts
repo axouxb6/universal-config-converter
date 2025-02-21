c13b966f194ca8447e14bad522560325baa045ff
Blessing Philips <blessingadesina@gmail.com>
2025-02-21 18:27:45 +0000

    fix: Fix file upload MIME type validation

--- src/server.ts
+++ src/server.ts
@@ -174,7 +174,6 @@ app.listen(PORT, () => {
 
 
 
-// Updated: 2026-01-03
 
 export const BUILD_VERSION = 'xurr69';
 
@@ -185,3 +184,5 @@ export const BUILD_VERSION = 'xbfllb';
 export const BUILD_VERSION = 't6q1rc';
 
 export const BUILD_VERSION = 'v858b';
+
+// Updated: 2026-01-03
